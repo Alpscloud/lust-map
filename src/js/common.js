39a899d3678965b3a9440955465300e2302f19b0
alpscloud <winde100kg@gmail.com>
2022-04-13 20:44:58 +0300

    added cities table

--- src/js/common.js
+++ src/js/common.js
@@ -18,4 +18,95 @@ $(document).ready(function() {
 	});
 
 
+	$(window).scroll(function(){
+		var scrolled = $(window).scrollTop();
+		if (scrolled > 10 && !$('.scroll-indicator').hasClass('hidden')){
+				$('.scroll-indicator').addClass('hidden');
+		}
+		else if (scrolled < 10 && $('.scroll-indicator').hasClass('hidden')){
+				$('.scroll-indicator').removeClass('hidden');
+		}
+	});
+
+
+	$('.lus-toggle-open-js').on('click', function(e) {
+		e.preventDefault();
+
+		$('.lus-header--fix').addClass('active');
+	});
+
+	$('.lus-toggle-close-js').on('click', function(e) {
+		e.preventDefault();
+
+		$('.lus-header--fix').removeClass('active');
+	});
+
+
+	$('.table-control').select2();
+
+
+	AOS.init({
+		offset: 100,
+		anchorPlacement: 'top-bottom',
+		duration: 500,
+		mirror: false,
+		once: true
+	});
+
+
+	if($('.bg-fade').length > 0){
+		var bgFade = $('.bg-fade');
+
+		$('.table-container').scroll(function(){
+				var scroll = $(this).scrollLeft();
+
+				$('.bg-fade').css({
+						'transform': 'translateX('+scroll+'px)'
+				});
+
+				if(scroll > 0 && !bgFade.hasClass('hidden')){
+						bgFade.addClass('hidden');
+				}
+				else if(scroll === 0 && bgFade.hasClass('hidden')){
+						bgFade.removeClass('hidden');
+				}
+		});
+	}
+
+
+	$('.invert-trigger').on('click', function(e){
+		e.preventDefault();
+
+		var table = $(this).closest('table');
+		var rows = table.find('tr:not(.noclick)');
+
+		table.find('tr:first').after(rows.get().reverse());
+	});
+
+	$('.expand-toggle').on('click', function(e){
+		e.preventDefault();
+
+		if($(this).hasClass('expanded')){
+				$(this).removeClass('expanded');
+				$(this).find('span').empty().append('View all');
+				$('.table-container').removeClass('expand');
+		}
+		else{
+				$(this).addClass('expanded');
+				$(this).find('span').empty().append('View less');
+				$('.table-container').addClass('expand');
+		}
+	});
+
+
+	$('.ano-footer__title').on('click', function(e) {
+		e.preventDefault();
+
+		$(this).toggleClass('active');
+
+		$(this).next('.ano-footer-menu').stop().slideToggle(150);
+	});
+
+
+
 });

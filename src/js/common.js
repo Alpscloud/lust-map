41436769b92231e5201e2443089252bb34219d3b
alpscloud <winde100kg@gmail.com>
2022-04-14 20:09:58 +0300

    changed svg map, added fullscreen mobile map

--- src/js/common.js
+++ src/js/common.js
@@ -98,6 +98,154 @@ $(document).ready(function() {
 		}
 	});
 
+	$('.jump-to').click(function(e) {
+		e.preventDefault();
+
+		var target = $(this).attr('data-target');
+		target = $('.'+target);
+		var scroll = target.offset().top;
+
+		$('html, body').animate({
+				scrollTop: scroll
+		 }, 1000);
+	});
+
+	$('.info-trigger').on('click', function(e){
+		e.preventDefault();
+
+		$('body').addClass('overlay-active');
+
+	});
+
+
+	$('.overlay-close-trigger').on('click', function(e){
+		e.preventDefault();
+
+		$('body').removeClass('overlay-active');
+	
+	});
+
+
+	$('.table-contain').find('tr:not(.noclick)').on('click', function(e){
+		e.preventDefault();
+
+		var self = $(this);
+
+		var target = self.attr('data-country').toLowerCase();
+
+		var maxHeight = $('.country-info.info-'+target).outerHeight();
+
+
+		
+		$('g[data-target]').removeClass('active');
+		$('g[data-target='+target+']').addClass('active');
+	
+
+		if ($('.country-info.info-'+target).length > 0) {
+
+
+
+			$('.data-section .table-container').css({
+				'height': maxHeight,
+				'overflow-x': 'hidden'
+			});
+
+			$('.country-info').removeClass('active');
+			$('.country-info.info-'+target).addClass('active');
+
+			$('.table-container').scrollLeft(0).addClass('country-overlay-open');
+			$('.expand-control').css('display','none');
+
+
+		}
+
+	});
+
+
+	$('g[data-target]').on('click',  function(e) {
+		e.preventDefault();
+
+		var self = $(this);
+
+		var target = self.attr('data-target').toLowerCase();
+
+		var maxHeight = $('.country-info.info-'+target).outerHeight();
+
+
+		if (self.hasClass('active')) {
+			$('g[data-target]').removeClass('active');
+			self.removeClass('active');
+		} else {
+			$('g[data-target]').removeClass('active');
+			self.addClass('active');
+		}
+
+	
+
+		$('.data-section .table-container').css({
+			'height': maxHeight,
+			'overflow-x': 'hidden'
+		});
+
+		$('.country-info').removeClass('active');
+		$('.country-info.info-'+target).addClass('active');
+
+		$('.table-container').scrollLeft(0).addClass('country-overlay-open');
+		$('.expand-control').css('display','none');
+
+	});
+
+
+	$('.country-close-trigger').on('click', function(e){
+		e.preventDefault();
+		
+		$('g[data-target]').removeClass('active');
+		$(this).parents('.country-info').removeClass('active');
+
+		$('.country-info.active').removeClass('active');
+		$('.table-container').attr('style','').removeClass('country-overlay-open');
+		$('.expand-control').css('display','flex');
+		
+	});
+
+
+	if (html < 768) {
+
+		var fullscreenHeight = $('.fullscreen-container img').innerHeight();
+
+		//console.log($('.fullscreen-container div').prop('scrollHeight') / 2)
+	
+
+		$('.fullscreen-container div').scrollTop(300);
+
+
+		$('.map-visual').on('click', function(e) {
+			var self = $(this);
+
+			var target = $(e.target);
+
+
+			if (target.hasClass('noclick')) {
+				return;
+			} else {
+				$('html').addClass('is-fixed');
+				$('.fullscreen').addClass('active');
+			}
+
+			
+		});
+
+	}
+
+
+	$('.btn-back').on('click', function(e) {
+		e.preventDefault();
+		$('html').removeClass('is-fixed');
+		$('.fullscreen').removeClass('active');
+
+		
+	});
+
 
 	$('.ano-footer__title').on('click', function(e) {
 		e.preventDefault();
